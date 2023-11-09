2973cd97b3d0bea3470119af42a8f40c955972c3
amisha26 <amisha8299@gmail.com>
2023-11-08 21:39:58 -0500

    chat layout done

--- chat-app/server.js
+++ chat-app/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 
 const app = express()
@@ -12,17 +13,24 @@ const io = socketio(server);
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const  botName = 'ChatCord Bot';
+
 // run when a client connects
 io.on('connection', socket => {
     // welcome current user
-    socket.emit('message', 'Welcome to chatcord');
+    socket.emit('message', formatMessage(botName, 'Welcome to chatcord'));
 
     // broadcast when a user connects
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
 
     // runs when client disconnects
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
+    })
+    
+    // Listen to chatMessage
+    socket.on('chatMessage', msg => {
+        io.emit('message', formatMessage('USER', msg));
     })
 })
 
